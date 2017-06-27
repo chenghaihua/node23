2242ed9ed2330dc5798375f705dc1d182e9850b4
cheng <834037683@qq.com>
2017-06-27 19:18:36 +0800

    learn

--- node/1.js
+++ node/1.js
@@ -1,6 +1,7 @@
 var http=require("http");
 var server=http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write();
     res.end("start");
 });
 server.listen(3040,'localhost');
